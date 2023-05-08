dd806b90254d9b1ec4ffb620e32a83db9b01105c
Thyrexx <henry.russxy@gmail.com>
2023-05-08 13:19:35 +0200

    added random selector and random sequence

---
+++ addons/bahviourTree/src/nodes/composites/random_selector.gd
@@ -0,0 +1,17 @@
+extends CompositeNode
+
+class_name RandomSelector
+
+func tick(actor, blackboard:Dictionary) -> BTNodeState:
+	var child_array := get_children()
+	randomize()
+	child_array.shuffle()
+	var result
+	
+	for child in child_array:
+		result = child.tick(actor, blackboard)
+		
+		if result != BTNodeState.FAILURE:
+			return result
+	
+	return BTNodeState.FAILURE
