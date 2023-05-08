1a3cd70fbf206d9f8c307be0821ff929052e7f1b
Andrushka130 <andreschwabauer1@gmail.com>
2023-05-08 14:45:37 +0200

    leaf bug

--- addons/bahviourTree/src/nodes/leaves/leaf.gd
+++ addons/bahviourTree/src/nodes/leaves/leaf.gd
@@ -3,5 +3,5 @@ extends BTNode
 class_name LeafNode
 
 func _ready():
-	assert(get_child_count() == 0, "Leaf nodes are not supposed to have any children")
+	assert(get_child_count() != 0, "Leaf nodes are not supposed to have any children")
 
