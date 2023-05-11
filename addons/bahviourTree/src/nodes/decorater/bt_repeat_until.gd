4274a853bbb9387e5d98f3561818c581ea919b1a
Andrushka130 <andreschwabauer1@gmail.com>
2023-05-11 09:05:36 +0200

    Typo

--- addons/bahviourTree/src/nodes/decorater/bt_repeat_until.gd
+++ addons/bahviourTree/src/nodes/decorater/bt_repeat_until.gd
@@ -5,12 +5,12 @@ class_name RepeatUntil
 @export_enum("Success", "Failure") var until = 0
 @onready var expected = bool(until)
 
-func tick(actor, blackbpard:Dictionary) -> BTNodeState:
+func tick(actor, blackboard:Dictionary) -> BTNodeState:
 	var result = not expected
 	var child = get_child(0) as BTNode
 	
 	while result != expected:
-		result = child.tick(actor, blackbpard)
+		result = child.tick(actor, blackboard)
 		
 	return result
 	
